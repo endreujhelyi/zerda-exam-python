0ec9defca8f976a06caa0f316d385506fa1824df
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-12-05 09:47:43 +0100

    second task done and works

--- second.py
+++ second.py
@@ -4,3 +4,17 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+import os
+
+def letter_counter(file_name, word):
+    multiplier = 10
+    if not os.path.isfile(file_name):
+        return False
+    else:
+        text = open(file_name, 'w')
+        for _ in range(multiplier):
+            text.write(word)
+        text.close()
+        return True
+        
