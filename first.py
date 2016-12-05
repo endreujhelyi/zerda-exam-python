162abe4d20afeb75969206ff5dd53a0cf032cfc6
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-12-05 09:29:27 +0100

    1st task done and works

--- first.py
+++ first.py
@@ -2,3 +2,9 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+def even_elements(input_list):
+    if type(input_list) == list:
+        return [input_list[i] for i in range(len(input_list)) if i % 2 == 1]
+    else:
+        raise TypeError
